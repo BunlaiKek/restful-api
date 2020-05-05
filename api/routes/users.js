2c5bb9652fdc7c5e2b2065c286b0549e0b7df5cc
Bunlai kek <kek.bunlai@optistech.com>
2020-05-05 17:13:29 +0700

    video 12 still error

--- api/routes/users.js
+++ api/routes/users.js
@@ -50,6 +50,42 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.post("/login", (req, res, next) => {
+  User.find({
+      email: req.body.email
+    }).exec().then(users => {
+      if (users.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed!"
+        });
+      }
+      bcrypt.compare(req.body.password, users[0].password, (err, same) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed!"
+          });
+        }
+        if (same) {
+          return res.status(200).json({
+            message: "Auth successful."
+          });
+        }
+      })
+      return res.status(401).json({
+        message: "Auth failed!"
+      });
+
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+
 router.delete("/:userId", (req, res, next) => {
   User.deleteOne({
       _id: req.params.userId
